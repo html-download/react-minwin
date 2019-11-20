758b674dd63d7261e7b6f84053a1d59c2eee6502
html-download <47537095+html-download@users.noreply.github.com>
2019-11-20 18:39:56 +0530

    Update store.jsx

--- src/pages/list/store.jsx
+++ src/pages/list/store.jsx
@@ -5,32 +5,45 @@ const initialState ={
 	
 	restaurants:[],
 	citydata:[], 
+	filterCitydata:[],
+	filterpricedata:[]
 
 }
 
 const reducer = (state = initialState, action) =>{
 	
-	if(action.type === Constants.DATAFETCH){
+ if(action.type === Constants.DATAFETCH){
+   //console.log("{ ...state, restaurants: action.restaurants}", { ...state, restaurants: action.restaurants});
+	return { ...state, restaurants: action.restaurants};
+  }
 
-		//console.log("{ ...state, restaurants: action.restaurants}", { ...state, restaurants: action.restaurants});
-		return { ...state, restaurants: action.restaurants};
-	}
+  if(action.type === Constants.DATAFILTER){
+        if(action.checkvalue === true){
+              return { ...state, citydata: state.citydata.concat(action.checkid),
+                   filterCitydata: state.filterCitydata.concat(action.filterCity),
+				}
+            }
+               else if(action.checkvalue === false) {
+                   const valueToRemove = action.checkid
+		 			return { ...state, citydata: state.citydata.filter(item => item !== valueToRemove),
+		 				filterCitydata: state.filterCitydata.filter(item => item.city !== valueToRemove),
+		 			}
+			}
+	  return { ...state, citydata: action.checkid}
+    }
 
-		if(action.type === Constants.DATAFILTER){
-
+    if(action.type === Constants.DATAPRICE){
 			if(action.checkvalue === true){
 
-				return { ...state, citydata: state.citydata.push(action.checkid) }
-
+				return{...state, filterpricedata: state.filterpricedata.concat(action.filterprice)}
 			}
-			else if (state.citydata > 0){
-
-				return { ...state, citydata: state.citydata.slice(action.checkid) }
+			else if(action.checkvalue === false){
+				 const valueToRemove = action.checkid
+				return {...state, filterpricedata: state.filterpricedata.filter(item => item.price !== valueToRemove) }
 			}
-			return
 
-	}
 
+    }
 
 	return state
 }

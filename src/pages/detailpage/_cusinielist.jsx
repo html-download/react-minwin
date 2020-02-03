bcc6e1d049b4fd2280f8c2a3cc96e812070373c4
html-download <47537095+html-download@users.noreply.github.com>
2020-02-03 19:02:08 +0530

    Update _cusinielist.jsx

--- src/pages/detailpage/_cusinielist.jsx
+++ src/pages/detailpage/_cusinielist.jsx
@@ -1,44 +1,56 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+ import React, { Component } from 'react';
+ import { Link } from 'react-router-dom';
 
-class Cusinielist extends Component{
+ class Cusinielist extends Component{
 
 constructor(props){
-  super(props);
+   super(props);
   this.state = this.props
-  console.log("this.state.cusinedata", this.state.cusinedata);
+  console.log("stateama", this.state.cusinedata);
 }
+
 render(){
-   
-   const cusine_detail =  this.state.cusinedata
-   console.log("cusine_detail", cusine_detail);
+	
+	const ingredient =  this.state;
+  const title= this.state.cusinetitle
+  console.log("title", title);
+  console.log("ingredient", ingredient);
+
+
  return(
 
-        <div className="menu_list">
-                            
-                            <div className="img">
-                              <a href="menu-detail.html">
-                                <img src={cusine_detail.category_image} />
-                              </a>
-                            </div>
-                            <h5>
-                              <a href="menu-detail.html">{cusine_detail.category_name}</a>
-                            </h5>
-                            <p className="grey_text">
-                              Lorem ipsum dolor sit amet, consectetur adipiscing
-                              elit. Mauris malesuada mollis risus, ut ultricies
-                              nisl viverra.
-                            </p>
-                            <span className="price">SAR 50.00</span>
-                            <a
-                              data-toggle="modal"
-                              data-target="#modal_ingredients"
-                              className="btn"
-                            >
-                              <i className="fa fa-plus-circle" /> Add
-                            </a>
-       </div>
-  );        
+ 		<div>
+ 				 <div className="menu_list">
+                <h6>{title}</h6>
+                            	<div>
+                                {ingredient.cusinedata.category_name} 
+                              </div>
+                             <div className="img">
+                               <a href="menu-detail.html">
+                               <img src={ingredient.cusinedata.category_image}/>
+                                    
+                               </a>
+                             </div>
+                             <h5>
+                             <a href="menu-detail.html"></a>
+
+                             </h5>
+                             <p className="grey_text">
+                                   {ingredient.cusinedata.category_content}
+                             </p>
+                             <span className="price">SAR {ingredient.cusinedata.category_price}</span>
+
+                             <a
+                               data-toggle="modal"
+                               data-target="#modal_ingredients"
+                             className="btn"
+                             >
+                               <i className="fa fa-plus-circle" /> Add
+                             </a>
+
+                      </div>
+ 		</div>
+  );				
  }
 }
 

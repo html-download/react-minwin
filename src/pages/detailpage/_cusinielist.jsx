47abc7896a5e27b495a8d712bd6c0fc03fe46abb
html-download <47537095+html-download@users.noreply.github.com>
2020-02-14 18:55:03 +0530

    Update _cusinielist.jsx

--- src/pages/detailpage/_cusinielist.jsx
+++ src/pages/detailpage/_cusinielist.jsx
@@ -1,25 +1,44 @@
  import React, { Component } from 'react';
  import { Link } from 'react-router-dom';
+ import { Button } from 'react-bootstrap';
+
 
  class Cusinielist extends Component{
 
 constructor(props){
    super(props);
   this.state = this.props
-  console.log("stateama", this.state.cusinedata);
+  console.log("this.state.show", this.state.show)
+
+  console.log("ingre_title", this.state.cusinetitle)
 }
 
 render(){
-	
-	const ingredient =  this.state;
+
+  const ingredient =  this.state;
   const title= this.state.cusinetitle
   console.log("title", title);
   console.log("ingredient", ingredient);
 
 
+
+const view_element = this.state.cusinedata.popup;
+console.log("view_element", view_element);
+const checkcry = view_element ? Object.keys(view_element).map((key) =>{
+            return <li key={key}>{`${key}-${view_element[key]} rs`}</li> 
+}): null
+
+/*const checkcry = view_element ? Object.keys(view_element).map((key) =>{
+            return <li key={key}>{view_element[key]}</li> 
+}): null*/
+ console.log("checkcry", checkcry);
+
+
+
  return(
 
  		<div>
+
  				 <div className="menu_list">
                 <h6>{title}</h6>
                             	<div>
@@ -39,21 +58,21 @@ render(){
                                    {ingredient.cusinedata.category_content}
                              </p>
                              <span className="price">SAR {ingredient.cusinedata.category_price}</span>
-
+                           
                              <a
-                               data-toggle="modal"
-                               data-target="#modal_ingredients"
-                             className="btn"
-                             >
-                               <i className="fa fa-plus-circle" /> Add
-                             </a>
-
-                      </div>
+                              data-toggle="modal"
+                              data-target="#modal_ingredients"
+                              className="btn"
+                              onClick={(e)=>this.state.showModal(checkcry)}
+                            >
+                            <i className="fa fa-plus-circle" /> Add
+                            </a>
+                             
+                                 
+       </div>
  		</div>
   );				
  }
 }
 
 export default Cusinielist;
-
-
